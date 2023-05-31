0a51747955fbe8aa635c6c3bc987fcb4fab46548
Ilya <ok.brain@yandex.ru>
2023-05-31 20:00:52 +0300

    dz

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+
+// int myFunc() {
+
+// int count = a;
+
+//   for (int i = 1; i < b; i++)
+//         {
+//         count = count * a;
+
+//         }
+        
+    
+
+//  return count;
+    
+// }
+// if(a > 1 && b > 1){
+//     int result = myFunc();
+
+// Console.WriteLine($"A в степени B = {result}");
+
+// } else{
+// Console.WriteLine("А и Б должны быть больше 0! ");
+// }
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите чило");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+
+
+// int sumFunc() {
+
+// while(num > 0) {
+
+     
+
+//         int number = num % 10;
+//         num = num / 10;
+//         sum = sum + number;
+// }
+// return sum;
+
+// }
+
+// Console.WriteLine(sumFunc());
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// void array () {
+
+//     int [] arr = new int [8];
+
+//     int count = 0;
+
+//     while(count < arr.Length) {
+//         arr[count] = new Random().Next(0,20);
+//         Console.Write($"{arr[count]} ");
+//         count++;
+//     }
+// }
+
+
+// array();
